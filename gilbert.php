6abb000e8131fa35499d8fa6eb292ffaef436b43
ALIHO GILBERT <alihogilbert@gmail.com>
2024-09-19 17:17:47 +0300

    Add files via upload

---
+++ gilbert.php
@@ -0,0 +1,19 @@
+<?php
+function call_name(){
+    $name = "ALIHO GILBERT";
+    return $name;
+}
+function my_age($current_year,$birth_year){
+    $age=$current_year-$birth_year;
+    return $age;
+}
+
+function my_home_address(){
+    $address="kyanamira-kabale";
+    return $address;
+}
+
+print("Welcome ".call_name());
+print("<br> to ".my_home_address()."");
+print (" <br>currently, you are ".my_age(2024,2002)." years old!");
+?>
